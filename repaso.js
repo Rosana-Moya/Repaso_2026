60b1b7bd618fd4f5d4e56b7bcd539b5d7f889c00
Rosana-Moya <rosanamoya88@gmail.com>
2026-04-24 21:53:52 +0200

    primer commit

---
+++ repaso.js
@@ -0,0 +1,10 @@
+alert("Hola");
+console.log("Funciona");
+
+document.querySelector("h1");
+
+let resultado = document.getElementById("resultado");
+
+for (let i = 1; i <= 10; i++) {
+  resultado.innerHTML += "5 x " + i + " = " + (5 * i) + "<br>";
+}
